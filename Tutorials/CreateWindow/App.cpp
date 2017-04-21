6ffb4622d1b3eb1e2a7f730be5d17f7a61fa5b64
ohrak22gmail <ohrak22@gmail.com>
2017-04-21 13:31:50 +0800

    Window Events
    윈도우 이벤트들

--- Tutorials/CreateWindow/App.cpp
+++ Tutorials/CreateWindow/App.cpp
@@ -11,9 +11,11 @@ using namespace Windows::ApplicationModel::Core;
 using namespace Windows::ApplicationModel::Activation;
 using namespace Windows::UI::Core;
 using namespace Windows::UI::Input;
+using namespace Windows::UI::Popups;
 using namespace Windows::System;
 using namespace Windows::Foundation;
 using namespace Windows::Graphics::Display;
+using namespace Platform;
 
 using Microsoft::WRL::ComPtr;
 
@@ -22,18 +24,23 @@ using Microsoft::WRL::ComPtr;
 App::App()
 {
 	m_windowClosed = false;
-
 }
 
 // IFrameworkView를 만들 때 호출되는 첫 번째 메서드입니다.
 void App::Initialize(CoreApplicationView^ applicationView)
 {
+	applicationView->Activated +=
+		ref new TypedEventHandler<CoreApplicationView^, IActivatedEventArgs^>(this, &App::OnActivated);
 }
 
 // CoreWindow 개체가 생성(또는 다시 생성)될 때 호출됩니다.
 void App::SetWindow(CoreWindow^ window)
 {
 	window->Closed += ref new TypedEventHandler<CoreWindow^, CoreWindowEventArgs^>(this, &App::OnWindowClosed);
+	window->PointerPressed += ref new TypedEventHandler<CoreWindow^, PointerEventArgs^>(this, &App::OnPointerPressed);
+	window->PointerWheelChanged += ref new TypedEventHandler<CoreWindow^, PointerEventArgs^>(this, &App::OnPointerWheelChanged);
+	window->KeyDown += ref new TypedEventHandler<CoreWindow^, KeyEventArgs^>(this, &App::OnKeyDown);
+	window->KeyUp += ref new TypedEventHandler<CoreWindow^, KeyEventArgs^>(this, &App::OnKeyUp);
 }
 
 // 장면 리소스를 초기화하거나 이전에 저장한 응용 프로그램 상태를 로드합니다.
@@ -44,9 +51,7 @@ void App::Load(Platform::String^ entryPoint)
 // 이 메서드는 창이 활성화된 후 호출됩니다.
 void App::Run()
 {
-	while (!m_windowClosed)
-	{
-	}
+	CoreWindow::GetForCurrentThread()->Dispatcher->ProcessEvents(CoreProcessEventsOption::ProcessUntilQuit);
 }
 
 // IFrameworkView에 필요합니다.
@@ -56,6 +61,33 @@ void App::Uninitialize()
 {
 }
 
+void App::OnPointerPressed(CoreWindow^ Window, PointerEventArgs^ Args)
+{
+	MessageDialog Dialog("OnPointerPressed", "OnPointerPressed!");
+	Dialog.ShowAsync();
+}
+void App::OnPointerWheelChanged(CoreWindow^ Window, PointerEventArgs^ Args)
+{
+	MessageDialog Dialog("Wheel: " + Args->CurrentPoint->Properties->MouseWheelDelta.ToString(), "OnPointerWheelChanged!");
+	Dialog.ShowAsync();
+}
+void App::OnKeyDown(CoreWindow^ Window, KeyEventArgs^ Args)
+{
+	MessageDialog Dialog("KeyDown: " + Args->VirtualKey.ToString(), "OnKeyDown!");
+	Dialog.ShowAsync();
+}
+void App::OnKeyUp(CoreWindow^ Window, KeyEventArgs^ Args)
+{
+	MessageDialog Dialog("OnKeyUp: " + Args->VirtualKey.ToString(), "OnKeyUp!");
+	Dialog.ShowAsync();
+}
+
+// 응용 프로그램 수명 주기 이벤트 처리기입니다.
+void App::OnActivated(CoreApplicationView^ applicationView, IActivatedEventArgs^ args)
+{
+	// CoreWindow가 활성화되어야 Run()이 시작됩니다.
+	CoreWindow::GetForCurrentThread()->Activate();
+}
 void App::OnWindowClosed(CoreWindow^ sender, CoreWindowEventArgs^ args)
 {
 	m_windowClosed = true;
--- Tutorials/CreateWindow/App.h
+++ Tutorials/CreateWindow/App.h
@@ -7,6 +7,9 @@ namespace CreateWindow
 	// 응용 프로그램의 주 진입점입니다. 응용 프로그램을 Windows 셸과 연결하고 응용 프로그램 수명 주기 이벤트를 처리합니다.
 	ref class App sealed : public Windows::ApplicationModel::Core::IFrameworkView
 	{
+	private:
+		bool m_windowClosed;
+
 	public:
 		App();
 
@@ -18,10 +21,14 @@ namespace CreateWindow
 		virtual void Uninitialize();
 
 	protected:
+		void OnActivated(Windows::ApplicationModel::Core::CoreApplicationView^ applicationView, Windows::ApplicationModel::Activation::IActivatedEventArgs^ args);
 		void OnWindowClosed(Windows::UI::Core::CoreWindow^ sender, Windows::UI::Core::CoreWindowEventArgs^ args);
+		void OnPointerPressed(Windows::UI::Core::CoreWindow^ Window, Windows::UI::Core::PointerEventArgs^ Args);
+		void OnPointerWheelChanged(Windows::UI::Core::CoreWindow ^ Window, Windows::UI::Core::PointerEventArgs ^ Args);
+		void OnKeyDown(Windows::UI::Core::CoreWindow^ Window, Windows::UI::Core::KeyEventArgs^ Args);
+		void OnKeyUp(Windows::UI::Core::CoreWindow^ Window, Windows::UI::Core::KeyEventArgs^ Args);
 
-	private:
-		bool m_windowClosed;
+	
 
 	};
 
