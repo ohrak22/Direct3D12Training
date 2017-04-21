01076df7377b4f10a3018bde9e91c0ba8424c98a
ohrak22gmail <ohrak22@gmail.com>
2017-04-21 14:04:29 +0800

    App Lifecycle Events and Loop
    어플리케이션 라이프 사이클 이벤트 및 루프

--- Tutorials/CreateWindow/App.cpp
+++ Tutorials/CreateWindow/App.cpp
@@ -29,8 +29,9 @@ App::App()
 // IFrameworkView를 만들 때 호출되는 첫 번째 메서드입니다.
 void App::Initialize(CoreApplicationView^ applicationView)
 {
-	applicationView->Activated +=
-		ref new TypedEventHandler<CoreApplicationView^, IActivatedEventArgs^>(this, &App::OnActivated);
+	applicationView->Activated += ref new TypedEventHandler<CoreApplicationView^, IActivatedEventArgs^>(this, &App::OnActivated);
+	CoreApplication::Suspending += ref new EventHandler<SuspendingEventArgs^>(this, &App::OnSuspending);
+	CoreApplication::Resuming += ref new EventHandler<Object^>(this, &App::OnResuming);
 }
 
 // CoreWindow 개체가 생성(또는 다시 생성)될 때 호출됩니다.
@@ -44,14 +45,28 @@ void App::SetWindow(CoreWindow^ window)
 }
 
 // 장면 리소스를 초기화하거나 이전에 저장한 응용 프로그램 상태를 로드합니다.
-void App::Load(Platform::String^ entryPoint)
+void App::Load(String^ entryPoint)
 {
 }
 
 // 이 메서드는 창이 활성화된 후 호출됩니다.
 void App::Run()
 {
-	CoreWindow::GetForCurrentThread()->Dispatcher->ProcessEvents(CoreProcessEventsOption::ProcessUntilQuit);
+	while(!m_windowClosed)
+	{
+		if (m_windowVisible)
+		{
+			CoreWindow::GetForCurrentThread()->Dispatcher->ProcessEvents(CoreProcessEventsOption::ProcessAllIfPresent);
+
+			// Run main code here.
+			// ...
+			// ...
+		}
+		else
+		{
+			CoreWindow::GetForCurrentThread()->Dispatcher->ProcessEvents(CoreProcessEventsOption::ProcessOneAndAllPending);
+		}
+	}
 }
 
 // IFrameworkView에 필요합니다.
@@ -61,6 +76,48 @@ void App::Uninitialize()
 {
 }
 
+// 응용 프로그램 수명 주기 이벤트 처리기입니다.
+void App::OnActivated(CoreApplicationView^ applicationView, IActivatedEventArgs^ args)
+{
+	// CoreWindow가 활성화되어야 Run()이 시작됩니다.
+	CoreWindow::GetForCurrentThread()->Activate();
+}
+void App::OnSuspending(Object^ sender, SuspendingEventArgs^ args)
+{
+	// 지연을 요청한 후 응용 프로그램 상태를 비동기적으로 저장합니다. 지연이 계속되는 것은
+	// 응용 프로그램이 일시 중단 중인 작업을 수행하는 중이라는 의미입니다.
+	// 지연이 무기한 지속되지 않을 수도 있습니다. 약 5초 후.
+	// 앱이 강제로 종료됩니다.
+	SuspendingDeferral^ deferral = args->SuspendingOperation->GetDeferral();
+
+	create_task([this, deferral]()
+	{
+		deferral->Complete();
+	});
+}
+void App::OnResuming(Object^ sender, Object^ args)
+{
+	// 일시 중단 시 언로드된 모든 데이터 또는 상태를 복원합니다. 기본적으로 데이터
+	// 및 상태는 일시 중단 이후 다시 시작할 때 유지됩니다. 이 이벤트는
+	// 응용 프로그램을 이전에 종료한 경우 발생하지 않습니다.
+
+}
+
+// 창 이벤트 처리기입니다.
+void App::OnWindowSizeChanged(CoreWindow^ sender, WindowSizeChangedEventArgs^ args)
+{
+}
+
+void App::OnVisibilityChanged(CoreWindow^ sender, VisibilityChangedEventArgs^ args)
+{
+	m_windowVisible = args->Visible;
+}
+void App::OnWindowClosed(CoreWindow^ sender, CoreWindowEventArgs^ args)
+{
+	m_windowClosed = true;
+}
+
+// Input Events.
 void App::OnPointerPressed(CoreWindow^ Window, PointerEventArgs^ Args)
 {
 	MessageDialog Dialog("OnPointerPressed", "OnPointerPressed!");
@@ -80,15 +137,4 @@ void App::OnKeyUp(CoreWindow^ Window, KeyEventArgs^ Args)
 {
 	MessageDialog Dialog("OnKeyUp: " + Args->VirtualKey.ToString(), "OnKeyUp!");
 	Dialog.ShowAsync();
-}
-
-// 응용 프로그램 수명 주기 이벤트 처리기입니다.
-void App::OnActivated(CoreApplicationView^ applicationView, IActivatedEventArgs^ args)
-{
-	// CoreWindow가 활성화되어야 Run()이 시작됩니다.
-	CoreWindow::GetForCurrentThread()->Activate();
-}
-void App::OnWindowClosed(CoreWindow^ sender, CoreWindowEventArgs^ args)
-{
-	m_windowClosed = true;
 }
--- Tutorials/CreateWindow/App.h
+++ Tutorials/CreateWindow/App.h
@@ -2,32 +2,41 @@
 
 #include "pch.h"
 
+using namespace Windows::ApplicationModel::Core;
+using namespace Windows::ApplicationModel::Activation;
+using namespace Windows::UI::Core;
+
 namespace CreateWindow
 {
 	// 응용 프로그램의 주 진입점입니다. 응용 프로그램을 Windows 셸과 연결하고 응용 프로그램 수명 주기 이벤트를 처리합니다.
-	ref class App sealed : public Windows::ApplicationModel::Core::IFrameworkView
+	ref class App sealed : public IFrameworkView
 	{
 	private:
 		bool m_windowClosed;
+		bool m_windowVisible;
 
 	public:
 		App();
 
 		// IFrameworkView 메서드.
-		virtual void Initialize(Windows::ApplicationModel::Core::CoreApplicationView^ applicationView);
-		virtual void SetWindow(Windows::UI::Core::CoreWindow^ window);
+		virtual void Initialize(CoreApplicationView^ applicationView);
+		virtual void SetWindow(CoreWindow^ window);
 		virtual void Load(Platform::String^ entryPoint);
 		virtual void Run();
 		virtual void Uninitialize();
 
 	protected:
-		void OnActivated(Windows::ApplicationModel::Core::CoreApplicationView^ applicationView, Windows::ApplicationModel::Activation::IActivatedEventArgs^ args);
-		void OnWindowClosed(Windows::UI::Core::CoreWindow^ sender, Windows::UI::Core::CoreWindowEventArgs^ args);
-		void OnPointerPressed(Windows::UI::Core::CoreWindow^ Window, Windows::UI::Core::PointerEventArgs^ Args);
-		void OnPointerWheelChanged(Windows::UI::Core::CoreWindow ^ Window, Windows::UI::Core::PointerEventArgs ^ Args);
-		void OnKeyDown(Windows::UI::Core::CoreWindow^ Window, Windows::UI::Core::KeyEventArgs^ Args);
-		void OnKeyUp(Windows::UI::Core::CoreWindow^ Window, Windows::UI::Core::KeyEventArgs^ Args);
+		void OnActivated(CoreApplicationView^ applicationView, IActivatedEventArgs^ args);
+		void OnSuspending(Platform::Object^ sender, Windows::ApplicationModel::SuspendingEventArgs^ args);
+		void OnResuming(Object^ sender, Object^ args);
+		void OnWindowSizeChanged(CoreWindow^ sender, WindowSizeChangedEventArgs^ args);
+		void OnVisibilityChanged(CoreWindow^ sender, VisibilityChangedEventArgs^ args);
+		void OnWindowClosed(CoreWindow^ sender, CoreWindowEventArgs^ args);
 
+		void OnPointerPressed(CoreWindow^ Window, PointerEventArgs^ Args);
+		void OnPointerWheelChanged(CoreWindow ^ Window, PointerEventArgs ^ Args);
+		void OnKeyDown(CoreWindow^ Window, KeyEventArgs^ Args);
+		void OnKeyUp(CoreWindow^ Window, KeyEventArgs^ Args);
 	
 
 	};
