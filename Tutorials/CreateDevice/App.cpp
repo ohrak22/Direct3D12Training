38dc788d27a130c74a2d3d5173c49c9941bb704f
ohrak22gmail <ohrak22@gmail.com>
2017-05-03 10:28:17 +0800

    Create Device
    디바이스 생성

--- Tutorials/CreateDevice/App.cpp
+++ Tutorials/CreateDevice/App.cpp
@@ -3,7 +3,7 @@
 
 #include <ppltasks.h>
 
-using namespace CreateWindow;
+using namespace CreateDevice;
 
 using namespace concurrency;
 using namespace Windows::ApplicationModel;
@@ -11,56 +11,75 @@ using namespace Windows::ApplicationModel::Core;
 using namespace Windows::ApplicationModel::Activation;
 using namespace Windows::UI::Core;
 using namespace Windows::UI::Input;
-using namespace Windows::UI::Popups;
 using namespace Windows::System;
 using namespace Windows::Foundation;
 using namespace Windows::Graphics::Display;
-using namespace Platform;
 
 using Microsoft::WRL::ComPtr;
 
-
-
-App::App()
+// main 함수는 IFrameworkView 클래스 초기화에만 사용됩니다.
+[Platform::MTAThread]
+int main(Platform::Array<Platform::String^>^)
 {
-	m_windowClosed = false;
+	auto direct3DApplicationSource = ref new Direct3DApplicationSource();
+	CoreApplication::Run(direct3DApplicationSource);
+	return 0;
+}
+
+IFrameworkView^ Direct3DApplicationSource::CreateView()
+{
+	return ref new App();
+}
+
+App::App() :
+	m_windowClosed(false),
+	m_windowVisible(true)
+{
 }
 
 // IFrameworkView를 만들 때 호출되는 첫 번째 메서드입니다.
 void App::Initialize(CoreApplicationView^ applicationView)
 {
+	// 응용 프로그램 수명 주기의 이벤트 처리기를 등록합니다. 이 예에는 Activated가 포함되므로
+	// CoreWindow를 활성 상태로 만들고 창에서 렌더링을 시작할 수 있습니다.
 	applicationView->Activated += ref new TypedEventHandler<CoreApplicationView^, IActivatedEventArgs^>(this, &App::OnActivated);
 	CoreApplication::Suspending += ref new EventHandler<SuspendingEventArgs^>(this, &App::OnSuspending);
-	CoreApplication::Resuming += ref new EventHandler<Object^>(this, &App::OnResuming);
+	CoreApplication::Resuming += ref new EventHandler<Platform::Object^>(this, &App::OnResuming);
+	m_device = std::make_shared<Device>();
+
+
 }
 
 // CoreWindow 개체가 생성(또는 다시 생성)될 때 호출됩니다.
 void App::SetWindow(CoreWindow^ window)
 {
+	window->SizeChanged += ref new TypedEventHandler<CoreWindow^, WindowSizeChangedEventArgs^>(this, &App::OnWindowSizeChanged);
+	window->VisibilityChanged += ref new TypedEventHandler<CoreWindow^, VisibilityChangedEventArgs^>(this, &App::OnVisibilityChanged);
 	window->Closed += ref new TypedEventHandler<CoreWindow^, CoreWindowEventArgs^>(this, &App::OnWindowClosed);
-	window->PointerPressed += ref new TypedEventHandler<CoreWindow^, PointerEventArgs^>(this, &App::OnPointerPressed);
-	window->PointerWheelChanged += ref new TypedEventHandler<CoreWindow^, PointerEventArgs^>(this, &App::OnPointerWheelChanged);
-	window->KeyDown += ref new TypedEventHandler<CoreWindow^, KeyEventArgs^>(this, &App::OnKeyDown);
-	window->KeyUp += ref new TypedEventHandler<CoreWindow^, KeyEventArgs^>(this, &App::OnKeyUp);
+
+	DisplayInformation^ currentDisplayInformation = DisplayInformation::GetForCurrentView();
+	currentDisplayInformation->DpiChanged += ref new TypedEventHandler<DisplayInformation^, Object^>(this, &App::OnDpiChanged);
+	currentDisplayInformation->OrientationChanged += ref new TypedEventHandler<DisplayInformation^, Object^>(this, &App::OnOrientationChanged);
+	DisplayInformation::DisplayContentsInvalidated += ref new TypedEventHandler<DisplayInformation^, Object^>(this, &App::OnDisplayContentsInvalidated);
+	m_device->SetWindow(window);
+
 }
 
 // 장면 리소스를 초기화하거나 이전에 저장한 응용 프로그램 상태를 로드합니다.
-void App::Load(String^ entryPoint)
+void App::Load(Platform::String^ entryPoint)
 {
 }
 
 // 이 메서드는 창이 활성화된 후 호출됩니다.
 void App::Run()
 {
-	while(!m_windowClosed)
+	while (!m_windowClosed)
 	{
 		if (m_windowVisible)
 		{
 			CoreWindow::GetForCurrentThread()->Dispatcher->ProcessEvents(CoreProcessEventsOption::ProcessAllIfPresent);
 
-			// Run main code here.
-			// ...
-			// ...
+			m_device->Present();
 		}
 		else
 		{
@@ -82,12 +101,12 @@ void App::OnActivated(CoreApplicationView^ applicationView, IActivatedEventArgs^
 	// CoreWindow가 활성화되어야 Run()이 시작됩니다.
 	CoreWindow::GetForCurrentThread()->Activate();
 }
-void App::OnSuspending(Object^ sender, SuspendingEventArgs^ args)
+void App::OnSuspending(Platform::Object^ sender, SuspendingEventArgs^ args)
 {
 	// 지연을 요청한 후 응용 프로그램 상태를 비동기적으로 저장합니다. 지연이 계속되는 것은
 	// 응용 프로그램이 일시 중단 중인 작업을 수행하는 중이라는 의미입니다.
 	// 지연이 무기한 지속되지 않을 수도 있습니다. 약 5초 후.
-	// 앱이 강제로 종료됩니다.
+	//앱이 강제로 종료됩니다.
 	SuspendingDeferral^ deferral = args->SuspendingOperation->GetDeferral();
 
 	create_task([this, deferral]()
@@ -95,17 +114,17 @@ void App::OnSuspending(Object^ sender, SuspendingEventArgs^ args)
 		deferral->Complete();
 	});
 }
-void App::OnResuming(Object^ sender, Object^ args)
+void App::OnResuming(Platform::Object^ sender, Platform::Object^ args)
 {
 	// 일시 중단 시 언로드된 모든 데이터 또는 상태를 복원합니다. 기본적으로 데이터
 	// 및 상태는 일시 중단 이후 다시 시작할 때 유지됩니다. 이 이벤트는
 	// 응용 프로그램을 이전에 종료한 경우 발생하지 않습니다.
-
 }
 
 // 창 이벤트 처리기입니다.
 void App::OnWindowSizeChanged(CoreWindow^ sender, WindowSizeChangedEventArgs^ args)
 {
+	//GetDeviceResources()->SetLogicalSize(Size(sender->Bounds.Width, sender->Bounds.Height));
 }
 void App::OnVisibilityChanged(CoreWindow^ sender, VisibilityChangedEventArgs^ args)
 {
@@ -116,24 +135,21 @@ void App::OnWindowClosed(CoreWindow^ sender, CoreWindowEventArgs^ args)
 	m_windowClosed = true;
 }
 
-// 입력 이벤트 처리기입니다.
-void App::OnPointerPressed(CoreWindow^ Window, PointerEventArgs^ Args)
+// DisplayInformation 이벤트 처리기입니다.
+void App::OnDpiChanged(DisplayInformation^ sender, Object^ args)
 {
-	MessageDialog Dialog("OnPointerPressed", "OnPointerPressed!");
-	Dialog.ShowAsync();
+	// 참고: 여기에서 검색된 LogicalDpi의 값은 고해상도 장치용으로 크기가 조정되는 경우
+	// 앱의 유효 DPI와 일치하지 않을 수 있습니다. DPI가 DeviceResources에서 설정되면
+	// 항상 GetDpi 메서드를 사용하여 DPI를 검색해야 합니다.
+	// 자세한 내용은 DeviceResources.cpp를 참조하세요.
+	//GetDeviceResources()->SetDpi(sender->LogicalDpi);
 }
-void App::OnPointerWheelChanged(CoreWindow^ Window, PointerEventArgs^ Args)
+void App::OnOrientationChanged(DisplayInformation^ sender, Object^ args)
 {
-	MessageDialog Dialog("Wheel: " + Args->CurrentPoint->Properties->MouseWheelDelta.ToString(), "OnPointerWheelChanged!");
-	Dialog.ShowAsync();
+	//GetDeviceResources()->SetCurrentOrientation(sender->CurrentOrientation);
 }
-void App::OnKeyDown(CoreWindow^ Window, KeyEventArgs^ Args)
+void App::OnDisplayContentsInvalidated(DisplayInformation^ sender, Object^ args)
 {
-	MessageDialog Dialog("KeyDown: " + Args->VirtualKey.ToString(), "OnKeyDown!");
-	Dialog.ShowAsync();
+	//GetDeviceResources()->ValidateDevice();
 }
-void App::OnKeyUp(CoreWindow^ Window, KeyEventArgs^ Args)
-{
-	MessageDialog Dialog("OnKeyUp: " + Args->VirtualKey.ToString(), "OnKeyUp!");
-	Dialog.ShowAsync();
-}
+
--- Tutorials/CreateDevice/App.h
+++ Tutorials/CreateDevice/App.h
@@ -1,22 +1,19 @@
 ﻿#pragma once
 
 #include "pch.h"
+#include "Device.h"
 
-namespace CreateWindow
+namespace CreateDevice
 {
 	// 응용 프로그램의 주 진입점입니다. 응용 프로그램을 Windows 셸과 연결하고 응용 프로그램 수명 주기 이벤트를 처리합니다.
-	ref class App sealed : public IFrameworkView
+	ref class App sealed : public Windows::ApplicationModel::Core::IFrameworkView
 	{
-	private:
-		bool m_windowClosed;
-		bool m_windowVisible;
-
 	public:
 		App();
 
 		// IFrameworkView 메서드.
-		virtual void Initialize(CoreApplicationView^ applicationView);
-		virtual void SetWindow(CoreWindow^ window);
+		virtual void Initialize(Windows::ApplicationModel::Core::CoreApplicationView^ applicationView);
+		virtual void SetWindow(Windows::UI::Core::CoreWindow^ window);
 		virtual void Load(Platform::String^ entryPoint);
 		virtual void Run();
 		virtual void Uninitialize();
@@ -32,14 +29,21 @@ namespace CreateWindow
 		void OnVisibilityChanged(Windows::UI::Core::CoreWindow^ sender, Windows::UI::Core::VisibilityChangedEventArgs^ args);
 		void OnWindowClosed(Windows::UI::Core::CoreWindow^ sender, Windows::UI::Core::CoreWindowEventArgs^ args);
 
-		// 입력 이벤트 처리기입니다.
-		void OnPointerPressed(CoreWindow^ Window, PointerEventArgs^ Args);
-		void OnPointerWheelChanged(CoreWindow ^ Window, PointerEventArgs ^ Args);
-		void OnKeyDown(CoreWindow^ Window, KeyEventArgs^ Args);
-		void OnKeyUp(CoreWindow^ Window, KeyEventArgs^ Args);
-	
+		// DisplayInformation 이벤트 처리기입니다.
+		void OnDpiChanged(Windows::Graphics::Display::DisplayInformation^ sender, Platform::Object^ args);
+		void OnOrientationChanged(Windows::Graphics::Display::DisplayInformation^ sender, Platform::Object^ args);
+		void OnDisplayContentsInvalidated(Windows::Graphics::Display::DisplayInformation^ sender, Platform::Object^ args);
 
+	private:
+		// m_deviceResources에 대한 전용 접근자는 장치 제거 오류로부터 보호합니다.
+		bool m_windowClosed;
+		bool m_windowVisible;
+		std::shared_ptr<Device> m_device;
 	};
-
-	
 }
+
+ref class Direct3DApplicationSource sealed : Windows::ApplicationModel::Core::IFrameworkViewSource
+{
+public:
+	virtual Windows::ApplicationModel::Core::IFrameworkView^ CreateView();
+};
